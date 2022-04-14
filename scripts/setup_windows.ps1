11ab447dc4ae0c9e798fd040e221d4530e1dac5c
yanshouwang <yanshouwang@outlook.com>
2022-04-14 15:31:32 +0800

    删除行尾空格

--- scripts/setup_windows.ps1
+++ scripts/setup_windows.ps1
@@ -2,6 +2,6 @@
 New-Item ../windows/include/wrapcv.h -ItemType HardLink -Value ../native/include/wrapcv.h
 New-Item ../windows/wrapcv.cpp -ItemType HardLink -Value ../native/wrapcv.cpp
 # Android
-New-Item ../android/src/main/cpp, ../android/src/main/cpp/include -ItemType Directory 
+New-Item ../android/src/main/cpp, ../android/src/main/cpp/include -ItemType Directory
 New-Item ../android/src/main/cpp/include/wrapcv.h -ItemType HardLink -Value ../native/include/wrapcv.h
 New-Item ../android/src/main/cpp/wrapcv.cpp -ItemType HardLink -Value ../native/wrapcv.cpp
